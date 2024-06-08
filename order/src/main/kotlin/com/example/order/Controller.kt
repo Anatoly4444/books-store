3bca7e2acb0529b2463c284516588b5a0122313f
anatolii.utkin <anatoly.ytkin@gmail.com>
2024-06-08 21:08:14 +0400

    created notification service

--- order/src/main/kotlin/com/example/order/Controller.kt
+++ order/src/main/kotlin/com/example/order/Controller.kt
@@ -1,15 +1,19 @@
 package com.example.order
 
 import org.springframework.http.ResponseEntity
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 @RestController("/order")
-class Controller {
+class Controller(var kafkaTemplate: KafkaTemplate<UUID, String>) {
 
     @PostMapping("/create/{bookName}")
     fun create(@PathVariable bookName: String) {
         print("Creating order $bookName")
+        kafkaTemplate.send("order", UUID.randomUUID(), bookName)
     }
 }
